e41add3621d91054a0377a7e910355aba0b495f0
Nand <nandpatel01218@gmail.com>
2025-11-16 11:29:57 +0530

    Many to Many Association

--- src/OOPs/Associations/Association.java
+++ src/OOPs/Associations/Association.java
@@ -70,9 +70,82 @@ class College {
 */
 
 
+// Many to Many
+class Student {
+    private final String name;
+    private List<Course> courses;
+    Student(String name){
+        this.name = name;
+        courses = new ArrayList<>();
+    }
+
+    public String getName(){
+        return name;
+    }
+
+    public void addCourse(Course course){
+        courses.add(course);
+    }
+
+    public void displayAllCourses(){
+        int numberOfStudents = courses.size();
+        System.out.println("All courses of " + name);
+        for(int i = 0; i < numberOfStudents; i++){
+            System.out.print(courses.get(i).getName() + " ");
+        }
+        System.out.println('\n');
+    }
+}
+class Course {
+    private String name;
+    private List<Student> students;
+
+    Course(String name){
+        this.name = name;
+        students = new ArrayList<>();
+    }
+
+    public String getName(){
+        return name;
+    }
+
+    public void addStudent(Student student){
+        students.add(student);
+    }
+
+    public void displayAllStudents(){
+        int numberOfStudents = students.size();
+        System.out.println("All Student of course " + name);
+        for(int i = 0; i < numberOfStudents; i++){
+            System.out.print(students.get(i).getName() + " ");
+        }
+        System.out.println("\n");
+    }
+}
+
+
 
 public class Association {
     public static void main(String[] args) {
+        Course cs = new Course("CS");
+        Course math = new Course("Math");
 
+        Student john = new Student("John");
+        Student sam = new Student("Sam");
+        Student smith = new Student("Smith");
+
+        cs.addStudent(john);
+        math.addStudent(john);
+        john.addCourse(cs);
+        john.addCourse(math);
+
+        math.addStudent(sam);
+        sam.addCourse(math);
+
+        cs.addStudent(smith);
+        smith.addCourse(cs);
+
+        john.displayAllCourses();
+        math.displayAllStudents();
     }
 }
