a7e9df66234fd4c4cfce1c6c33c628e9b48a0dff
Nand <nandpatel01218@gmail.com>
2025-11-16 23:08:07 +0530

    Cloning examples

--- src/OOPs/Basic.java
+++ src/OOPs/Basic.java
@@ -1,12 +1,8 @@
 package OOPs;
 
-import java.util.ArrayList;
 
 public class Basic {
     public static void main(String[] args) {
-        ArrayList<Integer> list = new ArrayList<>();
-        list.add(5);
-        System.out.println(list.add(8));;
-        System.out.println(list);
+
     }
 }
--- src/OOPs/Cloning/ObjectCloning.java
+++ src/OOPs/Cloning/ObjectCloning.java
@@ -1,6 +1,64 @@
 package OOPs.Cloning;
 
+import java.util.ArrayList;
+import java.util.List;
 
+// Example 1
+class Book implements Cloneable{
+    String title;
+    String author;
+
+    Book(String title, String author){
+        this.title = title;
+        this.author = author;
+    }
+
+    protected Object clone() throws CloneNotSupportedException {
+        return super.clone();
+    }
+}
+
+class Library implements Cloneable{
+    String name;
+    List<Book> book;
+
+    Library(String name) {
+        this.name = name;
+        book = new ArrayList<>();
+    }
+
+    public Library shallowClone() throws CloneNotSupportedException {
+        return (Library) super.clone();
+    }
+    public Library deepClone() throws CloneNotSupportedException {
+        Library clonedLib = (Library) super.clone();
+        List<Book> cloneBook = new ArrayList<>();
+        for(int i = 0; i < book.size(); i++){
+            cloneBook.add((Book)book.get(i).clone());
+        }
+        clonedLib.book = cloneBook;
+        return clonedLib;
+    }
+    public void display() {
+        int len = book.size();
+        for(int i = 0; i < len; i++){
+            System.out.print(book.get(i).title + " ");
+        }
+        System.out.println('\n');
+    }
+    public void addBook(Book newBook) {
+        book.add(newBook);
+    }
+
+    @Override
+    public Library clone() throws CloneNotSupportedException {
+        return (Library) super.clone();
+    }
+}
+
+
+
+// Example 2
 class Address implements Cloneable{
     String city;
 
